206d29dbb61426e3b5306bd5e196aeb96fbeb2af
Ali Faki <alifaki077@gmail.com>
2020-08-31 22:22:45 +0200

    a chest and two more levels

--- Torch/Torch.gd
+++ Torch/Torch.gd
@@ -3,19 +3,20 @@ extends Area2D
 export var active: bool = false
 export var always_active: bool = false
 var bodies = 0
+export var max_energy: float = 1.0
 
 func _ready() -> void:
 	if always_active:
 		active = true
 		$Sprite/Particles2D.emitting = true
-		$Light2D.energy = 1
+		$Light2D.energy = max_energy
 
 func _on_Torch_body_entered(body: Node) -> void:
 	if not active:
 		$SFX/TorchOpen.play()
 	active = true
 	$Sprite/Particles2D.emitting = true
-	$Light2D.energy = 1
+	$Light2D.energy = max_energy
 	bodies += 1
 	
 
@@ -24,4 +25,4 @@ func _on_Torch_body_exited(body: Node) -> void:
 	if bodies == 0 and not always_active:
 		active = false
 		$Sprite/Particles2D.emitting = false
-		$Light2D.energy = 0.7
+		$Light2D.energy = 0.5
